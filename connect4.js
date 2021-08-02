9638275ad0dcfdd2bfc4ec5d621998bb5771a0b3
Jimmy <jimjam5227@gmail.com>
2021-08-01 20:43:59 -0400

    bug fixes

--- connect4.js
+++ connect4.js
@@ -6,12 +6,12 @@
  */
 
 class Game {
-  constructor(p1, p2, WIDTH = 7, HEIGHT = 6) {
+  constructor(p1, p2, height = 6, width = 7) {
     this.players = [p1, p2];
-    this.WIDTH = WIDTH;
-    this.HEIGHT = HEIGHT;
-    this.currPlayer = p1; // active player: 1 or 2
-    this.makeBoard(); // array of rows, each row is array of cells  (board[y][x])
+    this.height = height;
+    this.width = width;
+    this.currPlayer = p1;
+    this.makeBoard();
     this.makeHtmlBoard();
     this.gameOver = false;
   }
@@ -21,12 +21,12 @@ class Game {
    */
   makeBoard() {
     this.board = [];
-    for (let y = 0; y < this.HEIGHT; y++) {
-      this.board.push(Array.from({ length: this.WIDTH }));
+    for (let y = 0; y < this.height; y++) {
+      this.board.push(Array.from({ length: this.width }));
     }
   }
 
-  /** makeHtmlBoard: make HTML table and row of column tops. */
+  /** makeHtmlBoard: make HTML table and row of column tops.  */
 
   makeHtmlBoard() {
     const board = document.getElementById("board");
@@ -36,10 +36,13 @@ class Game {
     const top = document.createElement("tr");
     top.setAttribute("id", "column-top");
 
+    // store a reference to the handleClick bound function
+    // so that we can remove the event listener correctly later
     this.handleGameClick = this.handleClick.bind(this);
-    top.addEventListener("click", this.handleClick);
 
-    for (let x = 0; x < this.WIDTH; x++) {
+    top.addEventListener("click", this.handleGameClick);
+
+    for (let x = 0; x < this.width; x++) {
       const headCell = document.createElement("td");
       headCell.setAttribute("id", x);
       top.append(headCell);
@@ -48,10 +51,10 @@ class Game {
     board.append(top);
 
     // make main part of board
-    for (let y = 0; y < this.HEIGHT; y++) {
+    for (let y = 0; y < this.height; y++) {
       const row = document.createElement("tr");
 
-      for (let x = 0; x < this.WIDTH; x++) {
+      for (let x = 0; x < this.width; x++) {
         const cell = document.createElement("td");
         cell.setAttribute("id", `${y}-${x}`);
         row.append(cell);
@@ -72,7 +75,7 @@ class Game {
     return null;
   }
 
-  /** placeInTable: update DOM to place piece into HTML table of board */
+  /** placeInTable: update DOM to place piece into HTML board */
 
   placeInTable(y, x) {
     const piece = document.createElement("div");
@@ -112,10 +115,11 @@ class Game {
     if (this.board.every((row) => row.every((cell) => cell))) {
       return this.endGame("Tie!");
     }
+
     // check for win
     if (this.checkForWin()) {
       this.gameOver = true;
-      return this.endGame(`Player ${this.currPlayer.color} won!`);
+      return this.endGame(`The ${this.currPlayer.color} player won!`);
     }
 
     // switch players
@@ -126,22 +130,21 @@ class Game {
   /** checkForWin: check board cell-by-cell for "does a win start here?" */
 
   checkForWin() {
+    // Check four cells to see if they're all color of current player
+    //  - cells: list of four (y, x) cells
+    //  - returns true if all are legal coordinates & all match currPlayer
     const _win = (cells) =>
-      // Check four cells to see if they're all color of current player
-      //  - cells: list of four (y, x) cells
-      //  - returns true if all are legal coordinates & all match currPlayer
-
       cells.every(
         ([y, x]) =>
           y >= 0 &&
-          y < this.HEIGHT &&
+          y < this.height &&
           x >= 0 &&
-          x < this.WIDTH &&
+          x < this.width &&
           this.board[y][x] === this.currPlayer
       );
 
-    for (let y = 0; y < this.HEIGHT; y++) {
-      for (let x = 0; x < this.WIDTH; x++) {
+    for (let y = 0; y < this.height; y++) {
+      for (let x = 0; x < this.width; x++) {
         // get "check list" of 4 cells (starting here) for each of the different
         // ways to win
         const horiz = [
@@ -189,8 +192,3 @@ document.getElementById("start-game").addEventListener("click", () => {
   let p2 = new Player(document.getElementById("p2-color").value);
   new Game(p1, p2);
 });
-
-// let myGame = new Game(6, 7);
-
-// myGame.makeBoard();
-// myGame.makeHtmlBoard();
